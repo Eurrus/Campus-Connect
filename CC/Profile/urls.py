44d33f7c080c7d313fba6d7e30ea3b954ad40123
Eurrus <vpavithra@students.vnit.ac.in>
2022-11-04 23:13:13 +0530

    basic users page added

--- CC/Profile/urls.py
+++ CC/Profile/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 from . import views
 app_name='Profile'
 urlpatterns=[
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    path('usersPage/', views.usersPage, name='usersPage'),
  ]
--- CC/Profile/views.py
+++ CC/Profile/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
    context={}
    return render(request, 'Profile/home.html',context)
+def usersPage(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'profile/usersPage.html', context)
