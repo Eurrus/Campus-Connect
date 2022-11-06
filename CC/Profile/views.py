89de0b867498a38f12b750f5598dd196254808b8
Eurrus <vpavithra@students.vnit.ac.in>
2022-11-06 09:38:28 +0530

    Added search filter in Users page and also fixed the href of Uers in other pages

--- CC/Profile/views.py
+++ CC/Profile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
 def home(request):
    context={}
@@ -9,3 +9,15 @@ def usersPage(request):
     users = User.objects.all()
     context = {'users': users}
     return render(request, 'profile/usersPage.html', context)
+def Ajax_searchUser(request):
+    q = request.GET.get('w')
+    results = User.objects.filter(username__icontains=q).distinct()
+    serialized_results = []
+    for result in results:
+        serialized_results.append({
+            'id': result.id,
+            'user_name': result.username,
+            })
+    print(serialized_results)
+
+    return JsonResponse({'results': serialized_results})
